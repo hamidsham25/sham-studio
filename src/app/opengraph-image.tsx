60a39f4907e71517f7ae4065c83391871abff7bc
hamidsham25 <hamid.sham@web.de>
2025-09-24 11:31:28 +0200

    edge warning fixed

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -1,7 +1,5 @@
 import { ImageResponse } from 'next/og'
 
-export const runtime = 'edge'
-
 export const alt = 'Sham Studio - Webdesign & Entwicklung in Hannover'
 export const size = {
   width: 1200,
